5372735504949340f24ae6a107941353d3c7776f
stevenvo780 <stevenvallejo780@gmail.com>
2020-04-09 12:15:40 -0500

    commit inicial

---
+++ src/Entity/Pedido.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PedidoRepository")
+ */
+class Pedido
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $customer_name;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private $customer_email;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $customer_mobile;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomerName(): ?string
+    {
+        return $this->customer_name;
+    }
+
+    public function setCustomerName(string $customer_name): self
+    {
+        $this->customer_name = $customer_name;
+
+        return $this;
+    }
+
+    public function getCustomerEmail(): ?string
+    {
+        return $this->customer_email;
+    }
+
+    public function setCustomerEmail(string $customer_email): self
+    {
+        $this->customer_email = $customer_email;
+
+        return $this;
+    }
+
+    public function getCustomerMobile(): ?string
+    {
+        return $this->customer_mobile;
+    }
+
+    public function setCustomerMobile(string $customer_mobile): self
+    {
+        $this->customer_mobile = $customer_mobile;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
